9f2072fb6eb4e7d12ed853a03a3b93afb3f19cfd
Márcio Diniz <marcio.sccp10@gmail.com>
2023-03-01 16:22:39 -0300

    Atts

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ const capaIniciar = document.querySelector('.capa-iniciar');
 
 function criarBlur() {
     capaIniciar.classList.add('anima-blur');
-    
+    setTimeout(() => {
+        capaIniciar.classList.add('anima-blur');
+    }, 300);
 }
+
+capaIniciar.addEventListener('click', criarBlur());
