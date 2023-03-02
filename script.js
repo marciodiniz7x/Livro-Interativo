d168b03cf8e01f13ed39b44e9b54ce8273d0c075
Márcio Diniz <marcio.sccp10@gmail.com>
2023-03-02 00:31:06 -0300

    Atts

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+const capa = document.querySelector('.capa');
+const pag01 = document.querySelector('.pag01');
 const capaIniciar = document.querySelector('.capa-iniciar');
+const pag01avance01 = document.querySelector('.pag01-avance01');
+const pag01return01 = document.querySelector('.pag01-return01');
 
-function criarBlur() {
-    capaIniciar.classList.add('anima-blur');
-    setTimeout(() => {
-        capaIniciar.classList.remove('anima-blur');
-    }, 300);
-}
+// Uma variável para se transformar no botão apropriado
+let botaoMomento = capaIniciar;
 
-capaIniciar.addEventListener('mouseover', criarBlur);
-capaIniciar.addEventListener('mouseout', criarBlur);
+
+// Uma função que irá avançar a página
+capaIniciar.addEventListener('click', () => {
+
+    capa.classList.add('sumir');
+    pag01.classList.remove('sumir');
+    
+});
+
+pag01return01.addEventListener('click', () => {
+
+    pag01.classList.add('sumir');
+    capa.classList.remove('sumir');
+   
+});
